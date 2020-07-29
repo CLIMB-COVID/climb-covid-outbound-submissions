7bf2546cf36df9c26be7126a7271b672c47a01c9
Sam Nicholls <samstudio8@gmail.com>
2020-07-29 15:06:51 +0000

    crappy script to generate ocarina bash cmd for marking submitted gisaid pags

---
+++ gisaid/gisaid_submitted_to_majora.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+import csv
+
+# usage: gisaid_submitted_to_majora.py <gisaid_undup_csv>
+
+for row in csv.DictReader(open(sys.argv[1])):
+    print("ocarina --env put publish --publish-group '%s' --service 'GISAID'" % row["pag_name"])
+
