3d1c2aed8d9977105bf12810074d2c02d4f78468
Sam Nicholls <samstudio8@gmail.com>
2020-08-11 15:30:16 +0000

    you fucking idiot

--- gisaid/outbound-gisaid-annouce.sh
+++ gisaid/outbound-gisaid-annouce.sh
@@ -1,6 +1,5 @@
 source ~/.ocarina
-#DATESTAMP=`date '+%Y%m%d'`
-DATESTAMP='20200810'
+DATESTAMP=`date '+%Y%m%d'`
 
 AUTHORS=`cut -f1 -d',' $COG_OUTBOUND_DIR/gisaid/$DATESTAMP/$DATESTAMP.gisaid.csv | sort | uniq -c | grep -v 'submitter'`
 SUBMISSIONS=`wc -l $COG_OUTBOUND_DIR/gisaid/$DATESTAMP/$DATESTAMP.gisaid.csv | cut -f1 -d' '`
