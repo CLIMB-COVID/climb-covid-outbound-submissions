9fa2c4f1d76a8822787876a648b2c65ffd9424ec
Sam Nicholls <samstudio8@gmail.com>
2020-10-06 21:12:32 +0000

    report ena status

--- ena/auto_submit.sh
+++ ena/auto_submit.sh
@@ -14,4 +14,17 @@ bind_ena_table_to_meta.py ena.csv $COG_PUBLISHED_DIR/majora.latest.metadata.tsv 
 
 cd $ELAN_SOFTWARE_DIR
 nextflow run dehuman.nf -c elan.config --manifest $COG_OUTBOUND_DIR/ena/$DATESTAMP/ena.nf.csv --datestamp $DATESTAMP --study PRJEB37886 --publish $ELAN_DIR --ascpbin $ASCP_BIN > $COG_OUTBOUND_DIR/ena/$DATESTAMP/nextflow.stdout
+ret=$?
+if [ $ret -ne 0 ]; then
+    lines=`tail -n 25 $COG_OUTBOUND_DIR/ena/$DATESTAMP/nextflow.stdout`
+else
+    lines=`awk -vRS= 'END{print}' $COG_OUTBOUND_DIR/ena/$DATESTAMP/nextflow.stdout`
+fi
+
+MSG='{"text":"*COG-UK ENA publishing pipeline finished...*
+...with exit status '"$ret"'
+'"\`\`\`${lines}\`\`\`"'"
+}'
+curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+
 mv .nextflow.log $COG_OUTBOUND_DIR/ena/$DATESTAMP/nextflow.log
