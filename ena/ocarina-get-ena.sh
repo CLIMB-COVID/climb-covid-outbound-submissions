acab810731286c1e16833459bc9d69cf25b57265
Sam Nicholls <samstudio8@gmail.com>
2021-01-29 13:06:09 +0000

    Migrate ocarina-get-ena to GET PAG v2 API

--- ena/ocarina-get-ena.sh
+++ ena/ocarina-get-ena.sh
@@ -2,7 +2,7 @@ ocarina --env get pag --test-name 'cog-uk-high-quality-public' --pass --private 
     --ffield-true owner_org_ena_opted \
     --ofield '~COG-UK/{central_sample_id}' ena_sample_name 'XXX' \
     --ofield central_sample_id central_sample_id 'XXX' \
-    --ofield submission_org_lab_or_name sample_center_name 'XXX' \
+    --ofield submission_org_credit_name sample_center_name 'XXX' \
     --ofield submission_org_code sample_center_code 'XXX' \
     --ofield collection_date collection_date '2020' \
     --ofield received_date received_date '' \
@@ -10,8 +10,8 @@ ocarina --env get pag --test-name 'cog-uk-high-quality-public' --pass --private 
     --ofield adm1_trans adm1 'XXX' \
     --ofield published_name published_name 'XXX' \
     --ofield alignment.current_path climb_fn 'XXX' \
-    --ofield owner_org_lab_or_name run_center_name 'XXX' \
+    --ofield credit_lab_name run_center_name 'XXX' \
     --ofield accession.gisaid.secondary_accession virus_identifier 'not provided' \
-    --ofield metric.ct.min_ct min_ct_value '' \
-    --ofield metric.ct.max_ct max_ct_value '' > ena.csv 2> err
+    --ofield min_ct min_ct_value '' \
+    --ofield max_ct max_ct_value '' > ena.csv 2> err
 
