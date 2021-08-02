a74ed5b4194ac127eceabcd30d2ee5c49d4550f9
Sam Nicholls <samstudio8@gmail.com>
2021-08-02 13:25:01 +0000

    Suppress messages about accessions table

--- accessions/export.sh
+++ accessions/export.sh
@@ -18,7 +18,8 @@ chmod 644 $COG_PUBLISHED_DIR/latest.accessions.tsv
 MSG="{'text':'
 *COG-UK accession table published* to \`$COG_PUBLISHED_DIR/latest.accessions.tsv\`'}"
 
-curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_OUTBOUND_HOOK
+# don bother telling slack anymore
+#curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_OUTBOUND_HOOK
 
 # Dump to s3
 s3cmd put --acl-public $COG_PUBLISHED_DIR/latest.accessions.tsv s3://cog-uk/accessions/latest.tsv
