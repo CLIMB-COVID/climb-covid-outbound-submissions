e11f5d13a483c56155d7d2f17812760426c570b1
Biowilko <sam1.wilkinson1@gmail.com>
2022-05-13 10:29:33 +0100

    make script executable, better newline handling

--- gisaid/remove_already_uploaded.py
+++ gisaid/remove_already_uploaded.py
@@ -3,7 +3,9 @@ import sys
 
 with open(sys.argv[1], "r") as accessions_tsv:
     submitted_cog_ids = set(
-        line.split("\t")[0] for line in accessions_tsv if len(line.split("\t")[3]) > 0
+        line.rstrip().split("\t")[0]
+        for line in accessions_tsv
+        if len(line.split("\t")[3]) > 0
     )
 
 with open(sys.argv[2], "r") as undup_manifest_ls:
@@ -12,7 +14,7 @@ with open(sys.argv[2], "r") as undup_manifest_ls:
         cog_id = gisaid_name.split("/")[2]
         if cog_id in submitted_cog_ids:
             print(
-                f"[NOTE] {gisaid_name.rstrip()} skipped as an artifact for this biosample has been uploaded previously",
+                f"[NOTE] {gisaid_name} skipped as an artifact for this biosample has been uploaded previously",
                 file=sys.stderr,
             )
             continue
