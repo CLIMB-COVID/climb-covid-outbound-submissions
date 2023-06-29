c6e6544f7a07afae28cdfa67ca90f77da191022d
Biowilko <sam1.wilkinson1@gmail.com>
2023-06-29 12:59:36 +0100

    Drop ena-assembly batch size

--- ena/auto_submit_assembly.sh
+++ ena/auto_submit_assembly.sh
@@ -12,7 +12,7 @@ conda activate $CONDA_OUTBOUND
 DATESTAMP=$1
 BEFORE_DATESTAMP=`date -d "$DATESTAMP -7 days" '+%Y-%m-%d'`
 WEBIN_JAR="$WEBIN_DIR/webin-cli-6.3.0.jar"
-BATCH_SIZE=40000
+BATCH_SIZE=25000
 
 NXF_WORK="/data/temp/nxf_work"
 
