611e6b73e58b27a780f6c7519127339ac74301ca
Biowilko <sam1.wilkinson1@gmail.com>
2023-06-30 10:59:54 +0100

    Check for accessions CSV

--- accessions/export.sh
+++ accessions/export.sh
@@ -15,21 +15,21 @@ cd $EAGLEOWL/data/outbound-submissions/accessions/
 
 # Retire the disgusting old dataview
 # ocarina --env --oauth get dataview --mdv COG2 -o cog2.mdv.json --task-wait --task-wait-attempts 100
+if [ ! -f "$DATESTAMP.accessions.csv" ]; then
+    ocarina --env --oauth get pag --test-name 'cog-uk-elan-minimal-qc' --pass --task-wait --task-wait-attempts 120 --odelimiter , \
+        --ofield central_sample_id central_sample_id 'XXX' \
+        --ofield anonymous_sample_id anonymous_sample_id '' \
+        --ofield published_date published_date 'XXX' \
+        --ofield accession.gisaid.primary_accession gisaid.accession '' \
+        --ofield accession.gisaid.secondary_accession gisaid.secondary_accession '' \
+        --ofield accession.ena-sample.primary_accession ena_sample.accession '' \
+        --ofield accession.ena-sample.secondary_accession ena_sample.secondary_accession '' \
+        --ofield accession.ena-run.primary_accession ena_run.accession '' \
+        --ofield accession.ena-run.secondary_accession ena_run.secondary_accession '' \
+        --ofield accession.ena-assembly.primary_accession ena_assembly.accession '' \
+        --ofield accession.ena-assembly.secondary_accession ena_assembly.secondary_accession '' > $DATESTAMP.accessions.csv 2> err
 
-ocarina --env --oauth get pag --test-name 'cog-uk-elan-minimal-qc' --pass --task-wait --task-wait-attempts 120 --odelimiter , \
-    --ofield central_sample_id central_sample_id 'XXX' \
-    --ofield anonymous_sample_id anonymous_sample_id '' \
-    --ofield published_date published_date 'XXX' \
-    --ofield accession.gisaid.primary_accession gisaid.accession '' \
-    --ofield accession.gisaid.secondary_accession gisaid.secondary_accession '' \
-    --ofield accession.ena-sample.primary_accession ena_sample.accession '' \
-    --ofield accession.ena-sample.secondary_accession ena_sample.secondary_accession '' \
-    --ofield accession.ena-run.primary_accession ena_run.accession '' \
-    --ofield accession.ena-run.secondary_accession ena_run.secondary_accession '' \
-    --ofield accession.ena-assembly.primary_accession ena_assembly.accession '' \
-    --ofield accession.ena-assembly.secondary_accession ena_assembly.secondary_accession '' > cog_uk_accessions.csv 2> err
-
-$OUTBOUND_SOFTWARE_DIR/accessions/accessions_export_parser.py cog_uk_accessions.csv $ARTIFACTS_ROOT/accessions/$DATESTAMP.accessions.tsv 2> $EAGLEOWL_LOGS/accessions/$DATESTAMP.log
+$OUTBOUND_SOFTWARE_DIR/accessions/accessions_export_parser.py $DATESTAMP.accessions.csv $ARTIFACTS_ROOT/accessions/$DATESTAMP.accessions.tsv 2> $EAGLEOWL_LOGS/accessions/$DATESTAMP.log
 
 # $OUTBOUND_SOFTWARE_DIR/accessions/accessions_json_to_tsv.py cog2.mdv.json 'GISAID,ENA-SAMPLE,ENA-RUN,ENA-ASSEMBLY' > $DATESTAMP.accessions.tsv 2> $EAGLEOWL_LOGS/accessions/$DATESTAMP.log
 
